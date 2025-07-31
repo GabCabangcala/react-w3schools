b6971eb7449859701748b4b5ba2b1548f2a1e061
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-07-31 07:40:09 +0000

    test run

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,4 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 export default function DOSTLogin() {
   const [email, setEmail] = useState('');
